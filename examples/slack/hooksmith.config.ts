51c01c69858ea886251ec6610e8d29939c5fc7ec
Renato Golia <renato@golia.tech>
2026-09-01 16:15:28 +0200

    Show advanced Slack message options in example

--- examples/slack/hooksmith.config.ts
+++ examples/slack/hooksmith.config.ts
@@ -13,6 +13,17 @@ export default {
       token: Deno.env.get("SLACK_BOT_TOKEN")!,
       channel: Deno.env.get("SLACK_CHANNEL")!,
       text: (event) => `${event.data.title}\n\n${event.metadata?.url}`,
+      blocks: (event) => [{
+        type: "section",
+        text: {
+          type: "mrkdwn",
+          text: `*${event.data.title}*\n${event.metadata?.url}`,
+        },
+      }],
+      payload: {
+        unfurl_links: false,
+        unfurl_media: false,
+      },
     })],
   }],
 } satisfies Config<PageEvent>;
